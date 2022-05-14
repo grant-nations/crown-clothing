550f5a79b10242057c5657ee97314963799fc0f2
GRANT NATIONS <u1247784@umail.utah.edu>
2022-05-13 20:27:01 -0600

    reflect cart changes in dropdown

--- src/contexts/cart.context.js
+++ src/contexts/cart.context.js
@@ -1,13 +1,36 @@
 import {createContext, useState} from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === productToAdd.id);
+
+    if (existingCartItem) {
+        return cartItems.map((cartItem) =>
+            cartItem.id === productToAdd.id
+                ? {...cartItem, quantity: cartItem.quantity + 1}
+                : cartItem
+        );
+    } else
+        return [...cartItems, {...productToAdd, quantity: 1}];
+}
+
 export const CartContext = createContext({
     cartOpen: false,
-    setCartOpen: () => null
+    setCartOpen: () => {
+    },
+    cartItems: [],
+    addItemToCart: () => {
+    }
 });
 
 export const CartProvider = ({children}) => {
     const [cartOpen, setCartOpen] = useState(false);
-    const value = {cartOpen, setCartOpen};
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd));
+    }
+
+    const value = {cartOpen, setCartOpen, cartItems, addItemToCart};
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
