c797677d298b39fe121cd43f66ea38974b879486
GRANT NATIONS <u1247784@umail.utah.edu>
2022-05-13 19:47:31 -0600

    add cart dropdown

--- src/routes/navigation/navigation.component.js
+++ src/routes/navigation/navigation.component.js
@@ -1,12 +1,16 @@
 import {Fragment, useContext} from "react";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import {UserContext} from "../../contexts/user.context";
 import {signOutUser} from "../../utils/firebase/firebase.utils";
 import {Outlet, Link} from "react-router-dom";
 import "./navigation.styles.scss";
 import {ReactComponent as CrwnLogo} from "../../assets/crown.svg";
+import {CartContext} from "../../contexts/cart.context";
 
 const Navigation = () => {
 
+    const {cartOpen, setCartOpen} = useContext(CartContext);
     const {currentUser} = useContext(UserContext);
 
     return (
@@ -25,7 +29,9 @@ const Navigation = () => {
                             SIGN IN
                         </Link>
                     }
+                    <CartIcon onClick={() => setCartOpen(prev => !prev)}/>
                 </div>
+                {cartOpen && <CartDropdown/>}
             </div>
             <Outlet/>
         </Fragment>
