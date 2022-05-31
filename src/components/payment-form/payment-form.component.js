8b3f832248cbbc2d589cb2cf85a74e3b478c20d0
GRANT NATIONS <u1247784@umail.utah.edu>
2022-05-30 21:44:55 -0600

    add stripe integration (need to clean UI and add user)

---
+++ src/components/payment-form/payment-form.component.js
@@ -0,0 +1,54 @@
+import {CardElement, useElements, useStripe} from "@stripe/react-stripe-js";
+import Button from "../button/button.component";
+import {BUTTON_TYPE_CLASSES} from "../button/button.component";
+import {PaymentFormContainer, FormContainer} from "./payment-form.styles";
+
+const PaymentForm = () => {
+
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const paymentHandler = async (e) => {
+        e.preventDefault();
+
+        if (!stripe || !elements)
+            return;
+
+        const response = await fetch("/.netlify/functions/create-payment-intent", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({amount: 100000})
+        }).then(res => res.json());
+
+        const clientSecret = response.paymentIntent.client_secret
+        const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: "Grant Nations"
+                }
+            }
+        });
+
+        if(paymentResult.error){
+            alert(paymentResult.error);
+        } else {
+            if(paymentResult.paymentIntent.status === "succeeded")
+                alert("Payment Successful");
+        }
+    }
+
+    return (
+        <PaymentFormContainer>
+            <FormContainer onSubmit={paymentHandler}>
+                <h2>Credit Card Payment: </h2>
+                <CardElement/>
+                <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
+            </FormContainer>
+        </PaymentFormContainer>
+    )
+}
+
+export default PaymentForm;
